77690273ea7532d24b73ac7b091712906c33b578
Jesus David <jesusdavida.e2089@gmail.com>
2024-07-12 01:41:39 -0700

    Modificación de archivo preprocesartexto. Se creo el metodo para leer las etiquetas.txt y el método para unir los json con las etiquetas. El último sigue en proceso

--- preprocesartexto.py
+++ preprocesartexto.py
@@ -1,47 +1,75 @@
 import os
-from operator import index
-
 import pandas
 import pandas as pd
 import spacy
 from dotenv import load_dotenv
 
-#Cargar variables de entorno
-load_dotenv()
+# Ajustar las opciones de visualización
+pd.set_option('display.max_rows', None)  # Muestra todas las filas
+pd.set_option('display.max_columns', None)  # Muestra todas las columnas
+pd.set_option('display.width', None)  # Ajusta el ancho de la visualización
+pd.set_option('display.max_colwidth', None)  # Muestra el contenido completo de las columnas
 
+# Cargar variables de entorno
+load_dotenv()
 PATH_SUBJECTSTRAIN = os.getenv('PATH_SUBJECTSTRAIN')
+PATH_TRAIN = os.getenv('PATH_TRAIN')
 
 #nlp = spacy.load('es_core_news_sm')
 
-
-#a = pd.read_json(f"{PATH_SUBJECTSTRAIN}/subject334.json")
-#print(a.message)
-#print(os.listdir(PATH_SUBJECTSTRAIN))
-
-
-
-
+# Función para leer los archivos JSON y crear un DataFrame con el nombre del archivo y
+# los datos asociados
 def readJSONFiles():
     # Crear diccionarios para los nombres de archivos y la información que contienen
     names_files = []
     files_reads = []
 
+    # Obtener el nombre del archivo para cada archivo en la ruta TRAIN
     for name_file in os.listdir(PATH_SUBJECTSTRAIN):
-        # print(type(name_file))
+        # leer JSON
         file = pd.read_json(f"{PATH_SUBJECTSTRAIN}/{name_file}")
+        # Guardar nombre de archivo
         names_files.append(name_file)
+        # Guardar datos asociados
         files_reads.append(file)
-        # print(file)
-        # partialinput = pd.DataFrame({index=[0],file})
-        # i = 0
 
-        # input = pd.DataFrame(index=[0],partialinput.head)
-        # i = i + 1
-
+    # Crear dataframe de los datos
     input = pd.DataFrame({'name_file': names_files, 'file': files_reads})
 
-    premensaje = input[input['name_file'] == 'subject334.json']
-    mensaje = premensaje.iloc[0]['file']['message']
+    #print(input)
+    #premensaje = input[input['name_file'] == 'subject334.json']
+    #mensaje = premensaje.iloc[0]['file']['message']
+    #print(mensaje)
 
-    print(mensaje)
+    return input
 
+# Función para leer las etiquetas
+def readTXT():
+    trainLabel = pd.read_csv(PATH_TRAIN)
+    #print(trainLabel[trainLabel['label']==1])
+    #print(trainLabel)
+    return trainLabel
+
+# Unir las etiquetas con los datos
+def linkJSONLabel():
+    input = readJSONFiles()
+    trainLabel = readTXT()
+    linkedFile = pd.DataFrame
+
+    for index, fila in trainLabel.iterrows():
+       # print(subject)
+        #print(index)
+        #print(fila)
+        specdata = input[input['name_file'] == f'{fila['Subject']}.json']
+
+        #print(specdata)
+        label = trainLabel.loc[trainLabel['Subject'] != 'Subject', 'label']
+        linkedFile = pd.concat([specdata,label], ignore_index=True)
+
+
+    print(linkedFile)
+
+    # premensaje = input[input['name_file'] == 'subject334.json']
+    #TODO porque no imprimie todos los subject
+    #TODO crear un archivo para ver cómo está generando los datos
+linkJSONLabel()
