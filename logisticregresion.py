4080b28363347ca55da2715f336ee5ab39a1b0da
Jesus David <jesusdavida.e2089@gmail.com>
2024-07-25 00:37:15 -0700

    Modificacion de logistic regresion

--- logisticregresion.py
+++ logisticregresion.py
@@ -3,30 +3,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 import vectorizacion
 import pandas as pd
-import os
 import seaborn as sns
-from dotenv import load_dotenv
 import matplotlib.pyplot as plt
-from sklearn import tree
 import numpy as np
 
-# Cargar variables de entorno
-load_dotenv()
-PATH_FINALFILE = os.getenv('PATH_FINALFILE')
-
 def logisticRegresion():
-    # Obtener vectorizacion de los datos
-    xTrainDF, xTrialDF = vectorizacion.bagOfWords()
-
-    # Obtener labels
-    trainLabel = pd.read_json(f'{PATH_FINALFILE}trainCorpus.json')
-    trialLabel = pd.read_json(f'{PATH_FINALFILE}trialCorpus.json')
+    # Obtener vectorizacion de los datos y corpus
+    xTrainDF, xTrialDF, trainCorpus, trialCorpus = vectorizacion.bagOfWords()
 
     # Conjunto de datos entrenamiento prueba. 30% de los datos para prueba
     # xTrain, xTrial, yTrain, yTrial = train_test_split (xTrainDF, trainLabel['label'], test_size=.30)
 
     # Crear modelo
-    logisticReg = LogisticRegression().fit(xTrainDF, trainLabel['label'])
+    logisticReg = LogisticRegression().fit(xTrainDF, trialCorpus['label'])
 
     # Mostrar resultados
 
@@ -37,7 +26,7 @@ def logisticRegresion():
     print(logisticReg.predict(xTrainDF))
 
     # Reporte de clasificacion
-    reporte = classification_report(trialLabel['label'], logisticReg.predict(xTrialDF))
+    reporte = classification_report(trialCorpus['label'], logisticReg.predict(xTrialDF))
     print(reporte)
 
 logisticRegresion()
